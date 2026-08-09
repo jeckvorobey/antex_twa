c0eaa0b283f86eab489427815aa4a1f94728bba1
sergei  V <jeckvorobey69@gmail.com>
2026-08-09 13:53:24 +0300

    Добавь блокирующий запрос разрешения Telegram

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <router-view />
+  <router-view v-if="authStore.canUseApp" />
+  <telegram-write-access-gate v-else />
 </template>
+
+<script setup lang="ts">
+import TelegramWriteAccessGate from '@components/auth/TelegramWriteAccessGate.vue';
+import { useAuthStore } from '@stores/auth.store';
+
+const authStore = useAuthStore();
+</script>
